2d2d3940a96f9b27c0f2b18b4712f5820fa644bf
Konstantin <ikotpad@gmail.com>
2020-10-18 09:44:10 +0100

    add right side, fix pinout settings

--- nassun/mctrl.py
+++ nassun/mctrl.py
@@ -10,8 +10,8 @@ lf_motor_fw = 27
 lf_motor_bw = 22
 
 rt_motor_en = 23
-rt_motor_fw = 24
-rt_motor_bw = 25
+rt_motor_fw = 25
+rt_motor_bw = 24
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(lf_motor_en, GPIO.OUT)
@@ -21,6 +21,7 @@ GPIO.setup(lf_motor_fw, GPIO.OUT)
 GPIO.setup(lf_motor_bw, GPIO.OUT)
 GPIO.setup(rt_motor_en, GPIO.OUT)
 rt_speed = GPIO.PWM(rt_motor_en, 100)
+rt_speed.start(0)
 GPIO.setup(rt_motor_fw, GPIO.OUT)
 GPIO.setup(rt_motor_bw, GPIO.OUT)
 
@@ -31,6 +32,7 @@ def gpioInit():
 
 def carMove(speedLeft, speedRight):
     # Car contol function. +1 +1 - moving forward, 0,1 moving back and so one
+    # Left side
     if speedLeft == 0:
         # This is stop function
         print("Left enging stop")
@@ -54,15 +56,44 @@ def carMove(speedLeft, speedRight):
         GPIO.output(lf_motor_bw, GPIO.HIGH)
         # GPIO.output(lf_motor_en, GPIO.HIGH)
         # Set speed
-        speedLeft = speedLeft * -1
-        lf_speed.ChangeDutyCycle(speedLeft)
+        # speedLeft = speedLeft * -1
+        lf_speed.ChangeDutyCycle(speedLeft * -1)
+    # Right side
+    if speedRight == 0:
+        # This is stop function
+        print("Rhgt enging stop")
+        # GPIO.setup(lf_motor_en, GPIO.HIGH)
+        GPIO.output(rt_motor_fw, GPIO.LOW)
+        GPIO.output(rt_motor_bw, GPIO.LOW)
+        GPIO.output(rt_motor_en, GPIO.LOW)
+        rt_speed.ChangeDutyCycle(0)
+    elif speedRight > 0:
+        # Left
+        print("Rhgt enging moving forward")
+        GPIO.output(rt_motor_fw, GPIO.HIGH)
+        GPIO.output(rt_motor_bw, GPIO.LOW)
+        # GPIO.output(lf_motor_en, GPIO.HIGH)
+        # Set speed
+        rt_speed.ChangeDutyCycle(speedRight)
+    else:
+        # Right
+        print("Rhgt enging moving backward")
+        GPIO.output(rt_motor_fw, GPIO.LOW)
+        GPIO.output(rt_motor_bw, GPIO.HIGH)
+        # GPIO.output(lf_motor_en, GPIO.HIGH)
+        # Set speed
+        rt_speed.ChangeDutyCycle(speedRight * -1)
 
 
 # Test on
 print("Start testing")
-carMove(100, 0)
+carMove(50, 50)
 time.sleep(1)
-carMove(-100, 0)
+carMove(50, 0)
+time.sleep(1)
+carMove(0, 50)
+time.sleep(1)
+carMove(-50, -50)
 time.sleep(1)
 carMove(0, 0)
 
