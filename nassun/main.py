db7e23d28f08d3e7ad06c9b6f85099bdd409de63
Konstantin <ikotpad@gmail.com>
2020-10-25 11:00:42 +0100

    Change main from old LED to the new one

--- nassun/main.py
+++ nassun/main.py
@@ -2,21 +2,50 @@
 # Main file to
 # Prepreq
 # sudo apt-get install python-rpi.gpio
+# sudo pip3 install keyboard
 
-import RPi.GPIO as GPIO
-import time
-# Set Control pin
-led = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(led ,GPIO.OUT)
+import keyboard
 
-# Turn ON
-print("LED on")
-GPIO.output(led,GPIO.HIGH)
+# Speed for left and right
+# car_speed = [0, 0]
 
-time.sleep(1)
-# Trun OFF
-print("LED off")
-GPIO.output(led,GPIO.LOW)
+def move(car_speed):
+    if car_speed[0] == 0:
+        # This is stop function
+        print("Left enging stop")
+    elif car_speed[0] > 0:
+        # Left forward
+        print("Left enging moving forward")
+    else:
+        # Left backward
+        print("Left enging moving backward")
+    # Right side
+    if car_speed[1] == 0:
+        # This is stop function
+        print("Rhgt enging stop")
+    elif car_speed[1] > 0:
+        # Right forward
+        print("Rhgt enging moving forward")
+    else:
+        # Right backward
+        print("Rhgt enging moving backward")
 
-GPIO.cleanup()
+def car_fw():
+    move([1, 1])
+
+def car_lf():
+    move([1, 0])
+
+def car_rt():
+    move([0, 1])
+
+def car_st():
+    move([0, 0])
+
+keyboard.add_hotkey('up', car_fw)
+keyboard.add_hotkey('left', car_lf)
+keyboard.add_hotkey('right', car_rt)
+keyboard.add_hotkey('down', car_st)
+
+print("Press ESC to stop.")
+keyboard.wait('esc')
