bd2dddb9992e0501933e110896155bd451c72bcb
Konstantin <ikotpad@gmail.com>
2020-10-31 09:11:16 +0100

    Working service with streaming and buttons. Need add actions to the buttons (up, donw, left, right, stop).

--- nassun/cv2_test.py
+++ nassun/cv2_test.py
@@ -1,20 +1,44 @@
 #!/usr/bin/env python3
 
-import numpy as np
-import cv2
+from flask import Flask, render_template, Response, request
+from camera import VideoCamera
+import datetime
 
-cap = cv2.VideoCapture(0)
+app = Flask(__name__)
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
+@app.route('/')
+def index():
+    print(request.data)
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    #
+    if request.method == 'POST':
+        print(request.method)
+        print(request.form.to_dict())
+        if request.form.get('Up') == 'Up':
+            # pass
+            print("Encrypted")
+        elif  request.form.get('Down') == 'Down':
+            # pass # do something else
+            print("Decrypted")
+        else:
+            # pass # unknown
+            print("None of them!")            
+    elif request.method == 'GET':
+        # return render_template("index.html")
+        print("No Post Back Call")
+    return render_template('index.html', title='Robot Nassun', time=timeString)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-    #cv2.imshow('frame', gray)
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
