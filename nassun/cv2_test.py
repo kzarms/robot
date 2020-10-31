58272da358ab4f21e083bd9fa2cc918f4bc6d41e
Konstantin <ikotpad@gmail.com>
2020-10-31 14:26:01 +0100

    Add http://127.0.0.1:5000/json for test. Woks better and simpler. Start working on implementation.

--- nassun/cv2_test.py
+++ nassun/cv2_test.py
@@ -29,5 +29,16 @@ def command(cmd=None):
     # ser.write(camera_command)
     return response, 200, {'Content-Type': 'text/plain'}
 
+@app.route('/json')
+def json():
+    return render_template('json.html')
+
+#background process happening without any refreshing
+@app.route('/background_process_test')
+def background_process_test():
+    print("Hello")
+    return "nothing"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
