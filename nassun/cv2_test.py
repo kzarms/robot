2d95df333b0fbe07ac5ea46d5f438dae2d4775f7
Konstantin <ikotpad@gmail.com>
2020-10-31 14:14:13 +0100

    Workin example with buttons :)

--- nassun/cv2_test.py
+++ nassun/cv2_test.py
@@ -1,44 +1,33 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template, Response, request
-from camera import VideoCamera
-import datetime
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    # print(request.form.validate_on_submit())
-    # pippo =  request.args.getlist('name[]')
-    now = datetime.datetime.now()
-    timeString = now.strftime("%Y-%m-%d %H:%M")
-    #
-    print(request)
-    print(request.method)
-    print(request.form.to_dict())
-    if request.method == 'POST':
-        rDic = request.form.to_dict()
-        if 'up' in rDic:
-            print("Got up")
-        elif 'down' in rDic:
-            print("Got down") 
-        else:
-            print("Got wrong") 
-    elif request.method == 'GET':
-        # return render_template("index.html")
-        print("No Post Back Call")
-    return render_template('index.html', title='Robot Nassun', time=timeString)
+LEFT, RIGHT, UP, DOWN, RESET = "left", "right", "up", "down", "reset"
+AVAILABLE_COMMANDS = {
+    'Left': LEFT,
+    'Right': RIGHT,
+    'Up': UP,
+    'Down': DOWN,
+    'Reset': RESET
+}
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+@app.route('/')
+def execute():
+    return render_template('main.html', commands=AVAILABLE_COMMANDS)
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/<cmd>')
+def command(cmd=None):
+    if cmd == RESET:
+       camera_command = "X"
+       response = "Resetting ..."
+    else:
+        camera_command = cmd[0].upper()
+        response = "Moving {}".format(cmd.capitalize())
+
+    # ser.write(camera_command)
+    return response, 200, {'Content-Type': 'text/plain'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
